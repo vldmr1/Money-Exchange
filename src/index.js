bf5e57d80ec827f2453abffb008836f88f59329f
Vladimir Malygin <43152051+vldmr1@users.noreply.github.com>
2018-09-28 12:47:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+module.exports =  function makeExchange(currency) {
+    const result = {};
+    if (currency > 10000) {
+        result.error = 'You are rich, my friend! We don\'t have so much coins for exchange'; 
+    } else if (currency > 0) {
+        const h = Math.floor(currency / 50),
+              q = Math.floor((currency % 50) / 25),
+              d = Math.floor(((currency % 50) % 25) / 10),
+              n = Math.floor((((currency % 50) % 25) % 10) / 5),
+              p = Math.floor((((currency % 50) % 25) % 10) % 5);
+        if (h) {
+            result["H"] = h;
+        }
+        if (q) {
+            result["Q"] = q;
+        }
+        if (d) {
+            result["D"] = d;
+        }
+        if (n) {
+            result["N"] = n;
+        }
+        if (p) {
+            result["P"] = p;
+        }
+    }    
+    return result;
